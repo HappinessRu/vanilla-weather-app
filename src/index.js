19db62d9c98c61224f81bc4b08d2ba0ed7d0b5ed
HappinessRu <happinessrutendo@gmail.com>
2023-10-18 01:24:22 +0200

    added search engine

--- src/index.js
+++ src/index.js
@@ -37,8 +37,16 @@ function displayTemperature(response) {
   iconElement.setAttribute("src", `${response.data.condition.icon_url}`);
   iconElement.setAttribute("alt", `${response.data.condition.description}`);
 }
-let apiKey = "30obd20tbb4105040138d6f1055c37a6";
-let city = "Gweru";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "30obd20tbb4105040138d6f1055c37a6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Gweru");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
