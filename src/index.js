03b1736c1a7379cfd5f93f89c604cd68c27840da
HappinessRu <happinessrutendo@gmail.com>
2023-10-23 17:14:43 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -18,56 +18,52 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return `${day} ${hours}: ${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
   return days[day];
 }
 
 function displayForecast(response) {
-  let forecast = response.data;
-  console.log(response.data);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  //let forecastHTML = `<div class="row">`;
-  //forecast.forEach(function (forecastDay, index) {
-  //if (index < 6) {
-  //forecastHTML =
-  //forecastHTML +
-  //`
-  //<div class="col-2">
-  //<div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-  //<img
-  //src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png${
-  //forecastDay.weather[0].icon_url
-  //}@2x.png"
-  //alt=""
-  //width="42"
-  ///>
-  //<div class="weather-forecast-temperatures">
-  //<span class="weather-forecast-temperature-max"> ${Math.round(
-  //forecastDay.temp.max
-  //)}° </span>
-  //<span class="weather-forecast-temperature-min"> ${Math.round(
-  //forecastDay.temp.min
-  //)}° </span>
-  //</div>
-  //</div>
-  //`;
-  //}
-  //});
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+  <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+  <img
+  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+    forecastDay.condition.icon
+  }.png"
+  alt=""
+  width="42"
+  />
+  <div class="weather-forecast-temperatures">
+  <span class="weather-forecast-temperature-max"> ${Math.round(
+    forecastDay.temperature.maximum
+  )}° </span>
+  <span class="weather-forecast-temperature-min"> ${Math.round(
+    forecastDay.temperature.minimun
+  )}° </span>
+  </div>
+  </div>
+  `;
+  });
 
-  //forecastHTML = forecastHTML + `</div>`;
-  //forecastElement.innerHTML = forecastHTML;
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
-  let apiKey = "0obd20tbb4105040138d6f1055c37a6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "30obd20tbb4105040138d6f1055c37a6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
